ca6301b28d59afb48e05735ea7c67e9b34767f17
eli-mi98 <elisiavmi@gmail.com>
2023-06-07 12:26:58 +0200

    versio 2 ex3

--- exercici-3.php
+++ exercici-3.php
@@ -2,7 +2,7 @@
 
 $paraules = array("nena", "norma", "mona", "menjar"); 
 
-if(contenenMateixCaracter($paraules, "n")){
+if(contenenMateixCaracter($paraules, "z")){
     echo 'Totes les paraules contenen el mateix caràcter.';
 }else{
     echo 'No totes les paraules contenen el mateix caràcter.'; 
@@ -12,11 +12,9 @@ function contenenMateixCaracter($paraules, $caracter){
     foreach($paraules as $paraula){
         if(strpos($paraula, $caracter)===false){
             return false;
-        }else{
-        return true; 
         }
     }
+    return true; 
 }
-
 
 ?>
